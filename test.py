5d8bdfa202e04bbe75ae166751a96c42d6e2bd47
AnshN009 <anshn009@gmail.com>
2024-11-30 23:19:31 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,7 @@
 #Also test anything that is acting weird in here
 #Code file on CodeHS is at https://codehs.com/sandbox/anshnayak/the-cipher-tron-3000
 #This whole project is intimately related to the Codebusters event in Science Olympiad
-#By: Ansh Nayak
+#Also BTW we absolutely need to make this public and stuff
+#By: Ansh Nayak, in 9th grade
+#From Charlotte, North Carolina, USA
+#I should really watch the GitHub video Papa wanted me to watch
